15103be706f81f97c29cee9c0ffa661610905a08
isuk11 <102477009+isuk11@users.noreply.github.com>
2022-03-27 16:56:14 +0100

    Add files via upload

---
+++ newsfeed.json.js
@@ -0,0 +1,16 @@
+[
+    {
+        "id": "0",
+        "title": "Wimbledon",
+        "url": "https://www.indiansinglesuk.com/asian-dating/events/wimbledon/",
+        "picture": "",
+        "description": "Game, Set, Matched! Twenty of our Indian Singletons voyaged all the way to Wimbledon. As the Tennis legends battled it out, we sat and ate strawberries with cream...",
+    },
+    {
+        "id": "0",
+        "title": "Wimbledon",
+        "url": "https://www.indiansinglesuk.com/asian-dating/events/wimbledon/",
+        "picture": "",
+        "description": "Game, Set, Matched! Twenty of our Indian Singletons voyaged all the way to Wimbledon. As the Tennis legends battled it out, we sat and ate strawberries with cream...",
+    },
+]
